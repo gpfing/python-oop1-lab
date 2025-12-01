7bc356259a072ce111eb6a837f1513c4a03a2e93
Garrett Pfingston <gpfingston@gmail.com>
2025-12-01 17:39:37 -0600

    updated logic to pass tests

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+    
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, value):
+        if value not in ("Small", "Medium", "Large"):
+            print("size must be Small, Medium, or Large")
+        self._size = value
+            
+    size = property(get_size, set_size)
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
